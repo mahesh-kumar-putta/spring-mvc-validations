ee024476953cdeb1812041f68839ff7a60c946b9
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-06-08 15:08:00 +0530

    Added logic to trim whitespaces in incoming requests

--- src/main/java/com/practice/spring_mvc_valids/controller/CustomerController.java
+++ src/main/java/com/practice/spring_mvc_valids/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.practice.spring_mvc_valids.controller;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,6 +16,12 @@ import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
+	
+	@InitBinder
+	public void filterThings(WebDataBinder webDataBinder) {
+		StringTrimmerEditor editor = new StringTrimmerEditor(true);
+		webDataBinder.registerCustomEditor(String.class, editor);
+	}
 
 	@GetMapping("/")
 	public String getForm(Model model) {
@@ -23,6 +32,7 @@ public class CustomerController {
 	
 	@PostMapping("processForm")
 	public String validForm( @Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+		System.out.println("last name is | "+customer.getLastName());
 		if(bindingResult.hasErrors()) {
 			return "customer-form";
 		} else {
